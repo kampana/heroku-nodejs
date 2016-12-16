d37d8befefdfeeddb512433bd612920942709a3f
kampana <urishmueli@gmail.com>
2016-12-16 23:47:09 +0200

    Test 4

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.send("hello NEW-AUTO222 world");
+  response.send("hello NEW-AUTO222333 world");
 });
 
 app.listen(app.get('port'), function() {
