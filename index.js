f92a6430f6f01bf1a38f8c6b7c1ec07547b6059d
kampana <urishmueli@gmail.com>
2016-12-16 23:39:24 +0200

    Test2

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.send("hello NEWWWW world");
+  response.send("hello NEW-AUTO world");
 });
 
 app.listen(app.get('port'), function() {
