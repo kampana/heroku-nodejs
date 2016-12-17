5d33375683e95da2a295e12e61b3744b0249afb8
kampana <urishmueli@gmail.com>
2016-12-17 10:36:36 +0200

    Local array response

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 var express = require('express');
 var app = express();
 
+// Local data in case of db failure
+const data = [{
+	years: '2016 - Present',
+	title: 'Full Stack Developer',
+	content: ['Hands on development in Java', 'SaaS development via AWS services','AngularJS', 'Spring', 'Hibernate over MySQL', 'C#'],
+	imageName: 'panaya' }, 
+	{
+		years: '2013-2016',
+		title: 'Team Leader',
+		content: ['Leading 6-12 team members', 'Hands on development in Java', 'AngularJS'],
+		imageName: 'ibm'
+	},
+	{
+		years: '2010-2013',
+		title: 'Team Leader',
+		content: ['Leading 4 team members worldwide', 'Hands on development in Java','GUI development in SWING'],
+		imageName: 'ibm'
+	},
+	{
+		years: '2008-2010',
+		title: 'GUI Developer',
+		content: ['Hands on development in Java', 'GUI development in SWING'],
+		imageName: 'ibm'
+	}];
+
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
-  response.send("hello NEW-AUT world");
+	response.setHeader("Access-Control-Allow-Origin", "*"); // Otherwise we'll have an access-control... error
+    response.send(JSON.stringify(data));
 });
 
 app.listen(app.get('port'), function() {
